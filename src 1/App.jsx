85bee6e8c6e6f6f8efedc57f1fe8530603b16b7c
Shannqa <shannqa@gmail.com>
2024-02-03 17:21:06 +0100

    update

---
+++ src 1/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Component from './components/Component.jsx'
+import ImageGetter from "./components/ImageGetter.jsx"
+
+function App(props) {
+  return  (
+      <div className="a">
+          <ImageGetter />
+      </div>
+    )
+}
+
+export default App
