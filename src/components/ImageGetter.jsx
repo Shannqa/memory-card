5af91d0ef52850bb1c2895781954e146ea1315df
Shannqa <shannqa@gmail.com>
2024-02-04 21:22:01 +0100

    Fixing a bug with the site not deploying

--- src/components/ImageGetter.jsx
+++ src/components/ImageGetter.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Card from "./Card.jsx";
-import API_KEY from "../../key.js";
+import API_KEY1 from "../../key.js";
 import { v4 as uuidv4 } from "uuid";
 
 
 function ImageGetter({imgCount, images, setImages, onPick, fetchSwitch, setFetchSwitch}) {
 
   useEffect(() => {
-    fetch(`https://api.thecatapi.com/v1/images/search?size=small&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=${imgCount}&api_key=${API_KEY}`)
+    fetch(`https://api.thecatapi.com/v1/images/search?size=small&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=${imgCount}&api_key=${API_KEY1}`)
     .then((response) => response.json())
     .then((data) => {
       const imgList = data.map((obj) => {
@@ -20,6 +20,7 @@ function ImageGetter({imgCount, images, setImages, onPick, fetchSwitch, setFetch
       });
       
       setImages(imgList);
+      // console.log(process.env.API_KEY);
     });
   }, [imgCount, fetchSwitch]);
   
