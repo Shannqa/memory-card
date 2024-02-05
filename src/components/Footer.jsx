b07015df94107d07d12ebda8075416bdad7d8c00
Shannqa <shannqa@gmail.com>
2024-02-05 18:14:27 +0100

    Styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  
+  
+  return(
+    <div className="footer">
+      <p>Created by <a href="https://shannqa.netlify.app/">Shannqa</a></p>
+    </div>
+    
+  )
+}
+
+export default Footer
